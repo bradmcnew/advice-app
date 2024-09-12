51dcc25d771b7652956ce9d1f57bda4ebf6a5bd0
Brad McNew <mcnewbrad@gmail.com>
2024-09-12 10:55:37 -0400

    db server connect commit

---
+++ backend/src/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const dotenv = require("dotenv");
+const pool = require("./config/db");
+const userRoutes = require("./routes/api/userRoutes");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Set to true if you're using HTTPS
+  })
+);
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use("/api/users", userRoutes);
+
+module.exports = app;
---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,5 @@
+const testController = async (req, res, next) => {
+  res.json({ message: "Users endpoint" });
+};
+
+module.exports = testController;
